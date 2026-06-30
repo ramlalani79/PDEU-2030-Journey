a464b21132f14fc276a6badf31a4f8f6df0dfbbd
ramlalani79 <ramlalani922007@gmail.com>
2026-06-30 16:24:10 +0530

    Create prime num.py

---
+++ Day 13/prime num.py
@@ -0,0 +1,13 @@
+n = int(input("plz enter the num ="))
+
+if n<=1:
+    print("not prime")
+else:
+    for i in range(2,n):
+        if n%i==0:
+            print("not prime")
+            break
+
+    else:
+        print("prime")
+ 
